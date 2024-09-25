938acb9f42b397a93e59afc84f256c3601f7941d
Shasidharyadav <110014214+Shasidharyadav@users.noreply.github.com>
2024-09-26 01:23:45 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,17 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    rollupOptions: {
+      external: ['xlsx'], // Mark 'xlsx' as external
+    },
     outDir: 'dist',
   },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',  // Proxy requests to backend during development
+        target: 'http://localhost:5000', // Proxy API requests to backend during development
         changeOrigin: true,
-      }
-    }
-  }
+      },
+    },
+  },
 });
